104e809a795a238bfc98e6dd454006d8e55ef392
mishrabadal <149366948+mishrabadal@users.noreply.github.com>
2025-09-11 17:09:32 +0530

    kmp algorithm

--- 18.cpp
+++ 18.cpp
@@ -3,10 +3,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int getLPSLength(string &s)
+{
+    int n = s.size();
+    vector<int> lps(n, 0);
+    int pre = 0, suf = 1;
+    while (suf < n)
+    {
+        if (s[pre] == s[suf])
+        {
+            lps[suf] = pre + 1;
+            pre++;
+            suf++;
+        }
+        else
+        {
+            if (pre == 0)
+            {
+                lps[suf] = 0;
+                suf++;
+            }
+            else
+            {
+                pre=lps[pre-1];
+            }
+        }
+    }
+    return lps[n-1];
+}
 
 int main()
 {
-    
+
     return 0;
 }
