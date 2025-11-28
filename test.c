2b34b7d5d24027e79c28257e104e00a52bff51a1
mishrabadal <149366948+mishrabadal@users.noreply.github.com>
2025-11-28 08:14:26 +0530

    success

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+   
+    int x,y,z;
+    printf("enter first number : ");
+    scanf("%d",&x);
+    printf("enter second number : ");
+    scanf("%d",&y);
+    printf("enter third number : ");
+    scanf("%d",&z); 
+    int sum=x+y+z;   
+    printf("sum is : %d",sum);
+    
+    return 0;
+
+
+}
